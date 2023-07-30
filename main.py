2174921eb5940b14350894742e4996205145da0c
Steinway <adrian.montas232@gmail.com>
2023-07-30 19:12:54 +0800

    Adding form log in txt file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+import logging
 
 class CovidTracingApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("COVID Tracing App")
-        self.geometry("400x400")
+        self.geometry("400x500")
         
         self.form = CovidForm(self)
         self.form.pack(expand=True, fill='both')
@@ -14,6 +15,10 @@ class CovidForm(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         
+        self.name_var = tk.StringVar()
+        self.phone_var = tk.StringVar()
+        self.address_var = tk.StringVar()
+
         self.symptoms = {
             "Fever": tk.IntVar(),
             "Cough": tk.IntVar(),
@@ -26,6 +31,7 @@ class CovidForm(tk.Frame):
             "Chills": tk.IntVar(),
             "Nausea or Vomiting": tk.IntVar(),
             "Diarrhea": tk.IntVar(),
+            "None of the above": tk.IntVar(),
         }
 
         self.create_widgets()
@@ -33,6 +39,24 @@ class CovidForm(tk.Frame):
     def create_widgets(self):
         label = tk.Label(self, text="COVID Tracing App Form", font=("Helvetica", 16, "bold"))
         label.pack(pady=10)
+
+        name_label = tk.Label(self, text="Name:")
+        name_label.pack(anchor="w", padx=10)
+
+        name_entry = tk.Entry(self, textvariable=self.name_var)
+        name_entry.pack(fill='x', padx=10)
+
+        phone_label = tk.Label(self, text="Phone Number:")
+        phone_label.pack(anchor="w", padx=10)
+
+        phone_entry = tk.Entry(self, textvariable=self.phone_var)
+        phone_entry.pack(fill='x', padx=10)
+
+        address_label = tk.Label(self, text="Address:")
+        address_label.pack(anchor="w", padx=10)
+
+        address_entry = tk.Entry(self, textvariable=self.address_var)
+        address_entry.pack(fill='x', padx=10)
 
         for symptom, var in self.symptoms.items():
             checkbox = tk.Checkbutton(self, text=symptom, variable=var, onvalue=1, offvalue=0)
@@ -42,15 +66,30 @@ class CovidForm(tk.Frame):
         submit_button.pack(pady=20)
 
     def submit_form(self):
-        selected_symptoms = [symptom for symptom, var in self.symptoms.items() if var.get() == 1]
+        name = self.name_var.get()
+        phone = self.phone_var.get()
+        address = self.address_var.get()
+
+        selected_symptoms = [symptom for symptom, var in self.symptoms.items() if var.get() == 1 and symptom != "None of the above"]
+
+        if "None of the above" in selected_symptoms:
+            selected_symptoms = ["None of the above"]
+
         if selected_symptoms:
             symptoms_string = ", ".join(selected_symptoms)
-            message = f"Thank you for submitting the form.\nYou have selected the following symptoms: {symptoms_string}"
+            message = f"Thank you, {name}, for submitting the form.\nPhone: {phone}\nAddress: {address}\nYou have selected the following symptoms: {symptoms_string}"
         else:
-            message = "Thank you for submitting the form.\nYou have not selected any symptoms."
+            message = f"Thank you, {name}, for submitting the form.\nPhone: {phone}\nAddress: {address}\nYou have not selected any symptoms."
 
-        # Replace this print statement with your desired functionality, e.g., saving data to a file or sending it to a server.
-        print(message)
+        self.log_form_data(name, phone, address, selected_symptoms)
+        # You can also call other functions to perform additional actions here.
+
+    def log_form_data(self, name, phone, address, selected_symptoms):
+        logging.basicConfig(filename='form_logs.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+        logging.info(f"Name: {name}")
+        logging.info(f"Phone: {phone}")
+        logging.info(f"Address: {address}")
+        logging.info(f"Symptoms: {', '.join(selected_symptoms)}")
 
 
 if __name__ == "__main__":
